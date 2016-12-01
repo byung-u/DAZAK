172fac2ca3471bae28007196ea199c3bb40fad5b
Byungwoo Jeon <iam.byungwoo@gmail.com>
2016-12-01 17:30:40 +0900

    Create pyconkr_repo_count_check.py
    Tro to check repository and try to contribute.

---
+++ pyconkr_repo_count_check.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import telepot
+
+from requests import get
+from bs4 import BeautifulSoup
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+CURRENT_REPO = 10
+
+def send_telegram():
+    config = configparser.ConfigParser()
+    config.readfp(open('bot.ini'))
+    telegram_token = self.config.get('TOKEN', 'telegram')
+    chat_id = self.config.get('TOKEN', 'chat_id')
+    bot = telepot.Bot(telegram_token)
+    bot.sendMessage(chat_id, "Repository Add Check it pyconkr!!")
+
+
+def send_email():
+    me = 'byungwoo@notify.com'
+    you = 'iam.byungwoo@gmail.com'
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "pyconkr 리포지터리가 바꼈다."
+    msg['From'] = me
+    msg['To'] = you
+
+    answer = "가서 시작했나 확인하고 contribution 가능한지 찔러보삼"
+    part1 = MIMEText(answer, 'plain', 'UTF-8')
+    msg.attach(part1)
+    #print(msg.as_string())
+    #print('send success')
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
+
+
+
+if __name__ == '__main__':
+
+    url = "https://github.com/pythonkr"
+    r = get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    cnt = 0
+    for link in  soup.find_all('a'):
+        repo = link.get('href')
+        repo = str(repo)
+        if (repo.startswith('/pythonkr')) and (repo.count('/') == 2):
+            #print(repo)
+            cnt+=1
+
+    if (cnt < CURRENT_REPO):
+        send_telegram()
+        send_email()
+
+    sys.exit(0)
